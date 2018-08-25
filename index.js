be2a9c1710367d1d4f1d85c1201db382cc4f4d7e
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-25 18:10:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const token = 'YOUR_TOKEN_HERE'
+const token = '50542b37dfa270136f18e9ccbc85ee09e67aa451'
 fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
